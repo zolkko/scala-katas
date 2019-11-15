2c789e8e8586c8f9db9a178d9274edbe504bcbd9
zolkko <zolkko@gmail.com>
2019-11-15 16:47:12 +0300

    Print density chart in the terminal.

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -4,6 +4,7 @@ import cats.syntax.apply._
 import cats.syntax.functor._
 import cats.effect._
 import com.stripe.rainier.sampler.RNG
+import com.stripe.rainier.repl.{DensityPlot, plot1D}
 
 object Main extends IOApp with CommandLine with RainierSample {
 
@@ -12,8 +13,9 @@ object Main extends IOApp with CommandLine with RainierSample {
   def putStrLn(value: String): IO[Unit] = IO { println(value) }
 
   def program(args: List[String]): IO[Unit] = for {
-    x <- doRegression[IO]((0 until 100).map(x => x -> (x + 1)).toList, 4).map(_.sample(10))
-    _ <- putStrLn(s"Regression: $x")
+    x <- doRegression[IO]((0 until 100).map(x => x -> (x + 1)).toList, 4)
+    v <- IO { DensityPlot().plot1D(x.sample().map(_.toDouble)).mkString("\n") }
+    _ <- putStrLn(v)
   } yield ()
 
   def run(args: List[String]): IO[ExitCode] = {
