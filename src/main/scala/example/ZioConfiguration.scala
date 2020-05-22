f30ce7082e62408fa09ecc7110794738aa17a27b
zolkko <zolkko@gmail.com>
2020-05-22 23:32:37 +0300

    Add Configuration.Service instance

--- src/main/scala/example/ZioConfiguration.scala
+++ src/main/scala/example/ZioConfiguration.scala
@@ -1,6 +1,9 @@
 package example
 
-import zio.{Has, Task}
+import cats.syntax.apply._
+import com.monovore.decline.{Command, Opts}
+import zio.{Has, Task, ZIO, ZLayer}
+
 
 case class ApiConfig(endpoint: String, port: Int)
 case class DbConfig(url: String, user: String, password: String)
@@ -12,8 +15,45 @@ object configuration {
 
   object Configuration {
     trait Service {
-      val load: Task[Config]
+      def load(args: List[String]): Task[Config]
     }
   }
+
+  trait ZioCmd extends Configuration.Service {
+    private val endpointOpt: Opts[String] = Opts.argument[String](metavar = "endpoint")
+      .withDefault("localhost")
+      .map(_.trim)
+      .validate("endpoint must not be empty"){ _.nonEmpty }
+
+    private val portOpt: Opts[Int] = Opts.argument[Int](metavar = "port")
+      .withDefault(8080)
+      .validate("port must be positive") { _ > 0 }
+
+    private val urlOpt: Opts[String] = Opts.argument[String](metavar = "url")
+      .withDefault("localhost")
+      .map(_.trim)
+      .validate("url must not be empty") { _.nonEmpty }
+
+    private val userOpt: Opts[String] = Opts.argument[String](metavar = "user")
+      .withDefault("username")
+      .map(_.trim)
+      .validate("username must not be empty") { _.nonEmpty }
+
+    private val passwordOpt: Opts[String] = Opts.argument[String](metavar = "password")
+      .map(_.trim)
+      .withDefault("")
+
+    private val apiConfig: Opts[ApiConfig] = (endpointOpt, portOpt).mapN(ApiConfig.apply)
+
+    private val dbConfig: Opts[DbConfig] = (urlOpt, userOpt, passwordOpt).mapN(DbConfig.apply)
+
+    private val configOpt: Opts[Config] = (apiConfig, dbConfig).mapN(Config.apply)
+
+    private val command: Command[Config] = Command(name = "zio-example", header = "ZIO example command", helpFlag = true)(configOpt)
+
+    def load(args: List[String]): Task[Config] = ZIO.fromEither(command.parse(args)).mapError(CommandLineError)
+  }
+
+  val confLayer: ZLayer[Any, Nothing, Configuration] = ZLayer.succeed(new ZioCmd {})
 
 }
