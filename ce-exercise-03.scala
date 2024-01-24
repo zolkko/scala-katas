6ea16672bc06e303c8c1626c63d369ce41ecda11
zolkko <zolkko@gmail.com>
2024-01-24 15:18:30 +0100

    Add exercise 3

---
+++ ce-exercise-03.scala
@@ -0,0 +1,26 @@
+//> using toolkit org.typelevel:default
+
+import scala.util.{Success, Failure, Try}
+import cats.effect.IO
+
+
+def option2IO[A](value: Option[A])(ifEmpty: Throwable): IO[A] =
+  value.fold(IO.raiseError(ifEmpty))(IO(_))
+
+def try2IO[A](value: Try[A]): IO[A] =
+  value match
+    case Success(x) => IO(x)
+    case Failure(e) => IO.raiseError(e)
+
+def either2IO[A](value: Either[Throwable, A]): IO[A] =
+  value.fold(IO.raiseError, IO.delay)
+
+@main
+def main =
+  import cats.effect.unsafe.implicits.global
+  val program = for
+    _ <- option2IO(Some("test option"))(RuntimeException("error"))
+    _ <- try2IO(Try("test try"))
+    _ <- either2IO(Right("test either"))
+  yield ()
+  program.unsafeRunSync()
